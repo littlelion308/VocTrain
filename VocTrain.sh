e5af7eaa526fcb3d9f7ac1c3a82844fdd22c21d4
littlelion308 <littlelion308@gmx.de>
2019-09-08 16:58:04 +0200

    inicial commit: added script

---
+++ VocTrain.sh
@@ -0,0 +1,24 @@
+#! /usr/bin/bash
+# VocTrain 0.1
+
+
+function mainMenu() {
+	echo "VocTrain Main Menu
+
+	1) Add a vocabulary
+	2) Get Asked
+	3) View statistics
+	4) Exit
+	
+_________________________________________-" 
+}
+function install_first() {
+
+}
+mainMenu
+EXIT="false"
+while [ $EXIT = "false" ]
+do
+	nop	
+done 
+exit 0
