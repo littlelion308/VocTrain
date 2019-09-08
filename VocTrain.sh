ecb8af893c83a1dbc36a9fd0fa1d2d7c50ad0330
littlelion308 <littlelion308@gmx.de>
2019-09-08 17:51:00 +0200

    main menu working now, but still missing core functionality

--- VocTrain.sh
+++ VocTrain.sh
@@ -1,7 +1,19 @@
 #! /usr/bin/bash
-# VocTrain 0.1
+# VocTrain 0.2
 
 
+function add(){
+	clear
+	echo this is not done yet
+}
+function learn(){
+	clear
+	echo this is not done yet
+}
+function stats(){
+	clear
+	echo this is not done yet
+}
 function mainMenu() {
 	echo "VocTrain Main Menu
 
@@ -10,15 +22,37 @@ function mainMenu() {
 	3) View statistics
 	4) Exit
 	
-_________________________________________-" 
+_________________________________________
+"
+	read INPUT
+	case $INPUT in
+		1|[aA]|[1][] )
+			add
+			;;
+		2|[bB]|[2][] )
+			learn
+			;;
+		3|[cC]|[3][] )
+			stats
+			;;
+		4|[dD]|[4][] )
+			exit 0
+			;;
+	esac
 }
 function install_first() {
-
+	if [[ -e "~/.local/share/VocTrain/" ]]; then
+		return 1
+	else
+		mkdir ~/.local/share/VocTrain
+	fi
 }
+install_first
+clear
 mainMenu
 EXIT="false"
 while [ $EXIT = "false" ]
 do
-	nop	
+	mainMenu
 done 
 exit 0
